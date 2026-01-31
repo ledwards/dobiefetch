30500d971a8730155c9cfa3f561ebc6c5355a61f
Lee Edwards <leeredwards@gmail.com>
2026-01-30 17:20:11 -0800

    Make Vercel Express entrypoint detectable

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { createApp } from "./server/app.js";
+
+// Vercel entrypoint for the Express framework.
+void express;
+const app = createApp();
+
+export default app;
